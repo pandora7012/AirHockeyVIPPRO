241dcbfc801c6939723b65f70e8ce4a284a69497
Empire <56126766+Empire843@users.noreply.github.com>
2022-10-20 01:18:23 +0700

    fix error position puck

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -15,17 +15,40 @@ public class Paddle : MonoBehaviour
     {
     }
     
-    void Update()
+    void FixedUpdate()
     {
         
         Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        if (mousePos.x  < boundX.x || mousePos.x > boundX.y || mousePos.y < boundY.x || mousePos.y > boundY.y)
-            return;
-        
-        if (Input.GetMouseButton(0))
+        if (!(mousePos.x  < boundX.x || mousePos.x > boundX.y || mousePos.y < boundY.x || mousePos.y > boundY.y))
         {
-            rb.MovePosition(mousePos);
+           /* if (Input.GetMouseButton(0))
+            {*/
+                rb.MovePosition(mousePos);
+            /*}*/
         }
+        else
+        {
+            if ((mousePos.x < boundX.x &&  mousePos.y < boundY.x) || (mousePos.x < boundX.x && mousePos.y > boundY.y)
+                || (mousePos.x > boundX.y && mousePos.y < boundY.x) || (mousePos.x > boundX.y && mousePos.y > boundY.y))
+            {
+                return;
+            }
+            if(mousePos.x < boundX.x || mousePos.x > boundX.y)
+            {
+                var vector2 = rb.position;
+                vector2.y = mousePos.y;
+                rb.MovePosition(vector2);
+            }
+            if (mousePos.y < boundY.x || mousePos.y > boundY.y)
+            {
+                var vector2 = rb.position;
+                vector2.x = mousePos.x;
+                rb.MovePosition(vector2);
+            }
+            
+        }
+        
+        
         
     }
 }
